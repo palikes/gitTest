07e18c3ee91349de010f934162ca006f26bda467
Pavol Červenický <palikes@gmail.com>
2021-10-31 08:30:43 +0100

    Initial Commit

---
+++ gitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTest
+//
+//  Created by Pavol Cervenicky on 31.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Pavol Cervenicky on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
